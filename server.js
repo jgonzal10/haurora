82c7e06bb7cb2c6f087b745f466785b756fb1e7c
jennifer <jgonzal10@gmail.com>
2017-12-25 16:52:26 -0200

    'herokude'

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-// server.js
+
+//Install express server
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
+
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
