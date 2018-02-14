d1857ffc3b11aec30e1db84f883b3b1ad504a0c5
jennifer <jgonzal10@gmail.com>
2018-02-14 10:20:43 -0200

    'pipesort'

--- src/app/pipes/pipes.ts
+++ src/app/pipes/pipes.ts
@@ -14,14 +14,3 @@ export class FilterPipe implements PipeTransform {
             : items;
     }
 }
-
-@Pipe({
-    name: 'sortBy'
-})
-export class SortByPipe implements PipeTransform {
-    transform(items: any[], sortedBy: string): any {
-       // console.log('sortedBy', sortedBy);
-        
-        return items.sort((a, b) => {return b[sortedBy] - a[sortedBy]});
-    }
-}
