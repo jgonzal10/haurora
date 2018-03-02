3b41116ad1d6c00440ca375bd89972da7803cba0
jennifer <jgonzal10@gmail.com>
2018-03-02 19:30:31 -0300

    'personalizacion'

--- src/app/home/editschema/editschema.component.ts
+++ src/app/home/editschema/editschema.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
- import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
+
 @Component({
   selector: 'app-editschema',
   templateUrl: './editschema.component.html',
   styleUrls: ['./editschema.component.scss']
 })
 export class EditschemaComponent implements OnInit {
+ 	myForm: FormGroup;
 
   constructor(private route: ActivatedRoute,
-  private router: Router) { }
+  private router: Router,private _fb: FormBuilder) { }
 
   ngOnInit() {
+
+      this.myForm = this._fb.group({
+            medicine: ['1/FLUORACILO'],
+            date: ['08/12/2017'],
+            dose: ['50.0'],
+            volume: ['78']
+
+        });
+        }
   	
-  }
+
 
 }
