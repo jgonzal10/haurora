936489a1d9c062e5de615772a397d5b3f6dc5fc9
jennifer <jgonzal10@gmail.com>
2018-03-23 18:44:27 -0300

    'editschema'

--- src/app/home/editschema/editschema.component.ts
+++ src/app/home/editschema/editschema.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
+import { PersonalizedSchema } from '../../models/personalizedschema';
 
 @Component({
   selector: 'app-editschema',
@@ -10,6 +11,7 @@ import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Valida
 export class EditschemaComponent implements OnInit {
  	myForm: FormGroup;
 
+  personalizedSchemaValues: PersonalizedSchema[]=[];
   constructor(private route: ActivatedRoute,
   private router: Router,private _fb: FormBuilder) { }
 
@@ -22,6 +24,81 @@ export class EditschemaComponent implements OnInit {
             volume: ['78']
 
         });
+
+this.personalizedSchemaValues=[
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"FLUORACILO",cycle:1,applicationDay:1,applicationDate:"2018-06-08",theoricalDose:"4000", solutionId:3, finalVolume:"250",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"CISPLATINO",cycle:1,applicationDay:1,applicationDate:"2018-06-08",theoricalDose:"80", solutionId:1, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"ONDANSETRON",cycle:1,applicationDay:1,applicationDate:"2018-06-08",theoricalDose:"16", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"DEXAMETASONA",cycle:1,applicationDay:1,applicationDate:"2018-06-08",theoricalDose:"8", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:1,applicationDay:1,applicationDate:"2018-06-08",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"CLORURO DE POTASIO",cycle:1,applicationDay:1,applicationDate:"2018-06-08",theoricalDose:"10", solutionId:3, finalVolume:"250",deviceId:1}
+    ,{personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"SULFATO DE MAGNESIO",cycle:1,applicationDay:1,applicationDate:"2018-06-08",theoricalDose:"10", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"ONDANSETRON",cycle:1,applicationDay:1,applicationDate:"2018-06-08",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:1,applicationDay:3,applicationDate:"2018-06-09",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:1,applicationDay:2,applicationDate:"2018-06-09",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:1,applicationDay:3,applicationDate:"2018-06-10",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:1,applicationDay:2,applicationDate:"2018-06-10",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:1,applicationDay:2,applicationDate:"2018-06-11",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:1,applicationDay:2,applicationDate:"2018-06-12",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"FLUORACILO",cycle:2,applicationDay:1,applicationDate:"2018-06-22",theoricalDose:"4000", solutionId:3, finalVolume:"250",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"CISPLATINO",cycle:2,applicationDay:1,applicationDate:"2018-06-22",theoricalDose:"80", solutionId:1, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"ONDANSETRON",cycle:2,applicationDay:1,applicationDate:"2018-06-22",theoricalDose:"16", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"DEXAMETASONA",cycle:2,applicationDay:1,applicationDate:"2018-06-22",theoricalDose:"8", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:2,applicationDay:1,applicationDate:"2018-06-22",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"CLORURO DE POTASIO",cycle:2,applicationDay:1,applicationDate:"2018-06-22",theoricalDose:"10", solutionId:3, finalVolume:"250",deviceId:1}
+    ,{personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"SULFATO DE MAGNESIO",cycle:2,applicationDay:1,applicationDate:"2018-06-22",theoricalDose:"10", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"ONDANSETRON",cycle:2,applicationDay:1,applicationDate:"2018-06-22",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:2,applicationDay:3,applicationDate:"2018-06-23",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:2,applicationDay:2,applicationDate:"2018-06-23",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:2,applicationDay:3,applicationDate:"2018-06-24",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:2,applicationDay:2,applicationDate:"2018-06-24",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:2,applicationDay:2,applicationDate:"2018-06-25",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:2,applicationDay:2,applicationDate:"2018-06-26",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"FLUORACILO",cycle:3,applicationDay:1,applicationDate:"2018-07-06",theoricalDose:"4000", solutionId:3, finalVolume:"250",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"CISPLATINO",cycle:3,applicationDay:1,applicationDate:"2018-07-06",theoricalDose:"80", solutionId:1, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"ONDANSETRON",cycle:3,applicationDay:1,applicationDate:"2018-07-06",theoricalDose:"16", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"DEXAMETASONA",cycle:3,applicationDay:1,applicationDate:"2018-07-06",theoricalDose:"8", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:3,applicationDay:1,applicationDate:"2018-07-06",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"CLORURO DE POTASIO",cycle:3,applicationDay:1,applicationDate:"2018-07-06",theoricalDose:"10", solutionId:3, finalVolume:"250",deviceId:1}
+    ,{personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"SULFATO DE MAGNESIO",cycle:3,applicationDay:1,applicationDate:"2018-07-06",theoricalDose:"10", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"ONDANSETRON",cycle:3,applicationDay:1,applicationDate:"2018-07-06",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:3,applicationDay:3,applicationDate:"2018-07-07",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:3,applicationDay:2,applicationDate:"2018-07-07",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:3,applicationDay:3,applicationDate:"2018-07-08",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:3,applicationDay:2,applicationDate:"2018-07-08",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:3,applicationDay:2,applicationDate:"2018-07-09",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:3,applicationDay:2,applicationDate:"2018-07-10",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+        {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"FLUORACILO",cycle:3,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"4000", solutionId:3, finalVolume:"250",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"CISPLATINO",cycle:3,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"80", solutionId:1, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"ONDANSETRON",cycle:3,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"16", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"DEXAMETASONA",cycle:3,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"8", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:3,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"CLORURO DE POTASIO",cycle:3,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"10", solutionId:3, finalVolume:"250",deviceId:1}
+    ,{personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"SULFATO DE MAGNESIO",cycle:3,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"10", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"ONDANSETRON",cycle:3,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:3,applicationDay:3,applicationDate:"2018-07-21",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:3,applicationDay:2,applicationDate:"2018-07-21",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:3,applicationDay:3,applicationDate:"2018-07-22",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:3,applicationDay:2,applicationDate:"2018-07-22",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:3,applicationDay:2,applicationDate:"2018-07-23",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:3,applicationDay:2,applicationDate:"2018-07-23",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+     {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"FLUORACILO",cycle:4,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"4000", solutionId:3, finalVolume:"250",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"CISPLATINO",cycle:4,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"80", solutionId:1, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"ONDANSETRON",cycle:4,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"16", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"DEXAMETASONA",cycle:4,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"8", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:4,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:2,medicineGenericName:"CLORURO DE POTASIO",cycle:4,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"10", solutionId:3, finalVolume:"250",deviceId:1}
+    ,{personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"SULFATO DE MAGNESIO",cycle:4,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"10", solutionId:3, finalVolume:"250",deviceId:1},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"ONDANSETRON",cycle:4,applicationDay:1,applicationDate:"2018-07-20",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:4,applicationDay:3,applicationDate:"2018-07-21",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:4,applicationDay:2,applicationDate:"2018-07-21",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:2,schemaId:2,medicineId:2,medicineGenericName:"APREPITANT",cycle:4,applicationDay:3,applicationDate:"2018-07-22",theoricalDose:"310", solutionId:0, finalVolume:"",deviceId:1},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:4,applicationDay:2,applicationDate:"2018-07-22",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:4,applicationDay:2,applicationDate:"2018-07-23",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+    {personalizedSchemaId:1,patientId:1,schemaId:2,medicineId:1,medicineGenericName:"ONDANSETRON",cycle:4,applicationDay:2,applicationDate:"2018-07-24",theoricalDose:"8", solutionId:0, finalVolume:"",deviceId:10},
+];
+
+
         }
   	
 
