1d5cb0f05328b48b6eca9ceb1918fec121271c78
jennifer <jgonzal10@gmail.com>
2018-09-15 21:32:13 -0300

    'basicDemo'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyTableComponent } from './my-table/my-table.component';
-import { MatTableModule, MatPaginatorModule, MatSortModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,MatInputModule, MatNativeDateModule,MatDatepickerModule } from '@angular/material';
+import { MatTableModule, MatPaginatorModule, MatSortModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule,MatInputModule, MatNativeDateModule,MatDatepickerModule,MatMenuModule,MatRadioModule,MatCardModule,MatFormFieldModule,MatSelectModule,MatOptionModule } from '@angular/material';
 import { MyNavComponent } from './my-nav/my-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
@@ -40,11 +41,16 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,      
+    ReactiveFormsModule,
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
     LayoutModule,
     MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatMenuModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
@@ -53,6 +59,9 @@ const appRoutes: Routes = [
     HttpClientModule,
     MatNativeDateModule, 
     MatDatepickerModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatOptionModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
--- src/app/production/production.component.ts
+++ src/app/production/production.component.ts
@@ -1,12 +1,163 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Preparation {
+  preparationId: number;
+  patientName: string;
+  medicineId: number;
+  medicineName: string;
+  volume: string;
+  finalVolume: string;
+  medicineConcentration: string;
+  medicineDispensationId: number;
+  medicineDispensationQuantity: number;
+  solutionDispensationId: number;
+  solutionDispensationQuantity: number;
+  deviceDispensationId: number;
+  stability: number;
+  expirationDate: string;
+  state: string;
+  acummulatedDoses: string;
+  acummulatedApplications: string;
+
+}
+
+const ELEMENT_DATA: Preparation[] = [
+  {  preparationId: 1,
+  patientName:"Ana Gomez",
+  medicineId: 2,
+  medicineName: 'FLUORACILO',
+  medicineConcentration: '4000',
+  volume: '4000',finalVolume:'250',
+  medicineDispensationId: 2,
+  medicineDispensationQuantity: 1,
+  solutionDispensationId: 1,
+  solutionDispensationQuantity: 1,
+  deviceDispensationId: 1,
+  stability: 8,
+  expirationDate: '08/06/2018',
+  state: 'Progress',
+  acummulatedDoses: '4000',
+  acummulatedApplications: '1' },
+  {  preparationId: 2,
+  patientName:"Ana Gomez",
+  medicineId: 2,
+  medicineName: 'CISPLATINO',
+  medicineConcentration: '80',
+  volume: '80',finalVolume:'250',
+  medicineDispensationId: 2,
+  medicineDispensationQuantity: 1,
+  solutionDispensationId: 1,
+  solutionDispensationQuantity: 10,
+  deviceDispensationId: 10,
+  stability: 8,
+  expirationDate: '08/06/2018',
+  state: 'Progress',
+  acummulatedDoses: '80',
+  acummulatedApplications: '1' },{  preparationId: 3,
+  patientName:"Ana Gomez",
+  medicineId: 2,
+  medicineName: 'ONDANSETRON',
+  medicineConcentration: '16',
+  volume: '16',finalVolume:'250',
+  medicineDispensationId: 2,
+  medicineDispensationQuantity: 1,
+  solutionDispensationId: 1,
+  solutionDispensationQuantity: 1,
+  deviceDispensationId: 10,
+  stability: 8,
+  expirationDate: '08/06/2018',
+  state: 'Progress',
+  acummulatedDoses: '16',
+  acummulatedApplications: '1' },
+  {  preparationId: 4,
+  patientName:"Ana Gomez",
+  medicineId: 2,
+  medicineName: 'DEXAMETASONA',
+  medicineConcentration: '8',
+  volume: '8',finalVolume:'250',
+  medicineDispensationId: 2,
+  medicineDispensationQuantity: 1,
+  solutionDispensationId: 1,
+  solutionDispensationQuantity: 1,
+  deviceDispensationId: 10,
+  stability: 8,
+  expirationDate: '08/06/2018',
+  state: 'Progress',
+  acummulatedDoses: '8',
+  acummulatedApplications: '1' },
+  {  preparationId: 5,
+  patientName:"Ana Gomez",
+  medicineId: 2,
+  medicineName: 'APREPITANT',
+  medicineConcentration: '310',
+  volume: '310',finalVolume:'',
+  medicineDispensationId: 2,
+  medicineDispensationQuantity: 1,
+  solutionDispensationId: 1,
+  solutionDispensationQuantity: 1,
+  deviceDispensationId: 1,
+  stability: 8,
+  expirationDate: '08/06/2018',
+  state: 'Progress',
+  acummulatedDoses: '310',
+  acummulatedApplications: '1' },
+  { preparationId: 6,
+  patientName:"Ana Gomez",
+  medicineId: 2,
+  medicineName: 'CLORURO DE POTASIO',
+  medicineConcentration: '10',
+  volume: '10',finalVolume:'',
+  medicineDispensationId: 2,
+  medicineDispensationQuantity: 1,
+  solutionDispensationId: 1,
+  solutionDispensationQuantity: 1,
+  deviceDispensationId: 1,
+  stability: 8,
+  expirationDate: '08/06/2018',
+  state: 'Progress',
+  acummulatedDoses: '10',
+  acummulatedApplications: '1' },
+    { preparationId: 7,
+  patientName:"Ana Gomez",
+  medicineId: 2,
+  medicineName: 'SULFATO DE MAGNESIO',
+  medicineConcentration: '10',
+  volume: '10',finalVolume:'',
+  medicineDispensationId: 2,
+  medicineDispensationQuantity: 1,
+  solutionDispensationId: 1,
+  solutionDispensationQuantity: 1,
+  deviceDispensationId: 1,
+  stability: 8,
+  expirationDate: '08/06/2018',
+  state: 'Progress',
+  acummulatedDoses: '10',
+  acummulatedApplications: '1' },
+    { preparationId: 8,
+  patientName:"Ana Gomez",
+  medicineId: 2,
+  medicineName: 'SULFATO DE MAGNESIO',
+  medicineConcentration: '10',
+  volume: '10',finalVolume:'',
+  medicineDispensationId: 2,
+  medicineDispensationQuantity: 1,
+  solutionDispensationId: 1,
+  solutionDispensationQuantity: 1,
+  deviceDispensationId: 1,
+  stability: 8,
+  expirationDate: '08/06/2018',
+  state: 'Progress',
+  acummulatedDoses: '10',
+  acummulatedApplications: '1' }
+  ];
 @Component({
   selector: 'app-production',
   templateUrl: './production.component.html',
   styleUrls: ['./production.component.css']
 })
 export class ProductionComponent implements OnInit {
-
+  displayedColumns: string[] = ['patientName', 'medicineName', 'volume','solutionDispensationId', 'volume', 'deviceDispensationId','solutionDispensationQuantity', 'medicineConcentration', 'stability','expirationDate', 'state', 'acummulatedDoses','acummulatedApplications'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit() {
